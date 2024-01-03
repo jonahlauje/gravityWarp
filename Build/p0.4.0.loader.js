566a9df252ad467a2e4df261f78449c75d631435
jonahlauje <jonahlauje@hotmail.com>
2024-01-03 20:36:57 +1100

    0.4

--- Build/p0.4.0.loader.js
+++ Build/p0.4.0.loader.js
@@ -55,7 +55,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 77123788,
+    wasmFileSize: 77123731,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
